d891c078f6156b1f188456465432493c455003fa
Hugo Souza Araujo <hsaraujo98@outlook.com>
2025-11-25 12:19:41 -0300

    4. Retângulo Completo

--- Heranca/retangulo_completo/Retangulo.java
+++ Heranca/retangulo_completo/Retangulo.java
@@ -7,9 +7,6 @@ public class Retangulo extends FormaGeometrica {
     }
 
     public void setLargura(double largura) {
-        if (largura < 0) {
-            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
-        }
         this.largura = largura;
     }
 
@@ -18,9 +15,16 @@ public class Retangulo extends FormaGeometrica {
     }
 
     public void setAltura(double altura) {
-        if (altura < 0) {
-            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
-        }
         this.altura = altura;
+    }
+
+    @Override
+    public double area() {
+        return largura * altura;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Retangulo] %.2f / %.2f", largura, altura);
     }
 }
