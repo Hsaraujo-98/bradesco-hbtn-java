10fa66a89e61c604a6de042bc43711ebd88dd882
Hugo Souza Araujo <hsaraujo98@outlook.com>
2025-11-25 13:41:27 -0300

    6. Classe Abstrata

---
+++ Heranca/abstrata/Quadrado.java
@@ -0,0 +1,29 @@
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    public Quadrado() {
+        this.lado = 0;
+    }
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if (lado < 0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }
+        this.lado = lado;
+        this.largura = lado;
+        this.altura = lado;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Quadrado] %.2f", lado);
+    }
+}
