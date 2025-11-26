dff4101184d7b9f136f2cae3979dc05cfa10f1a1
Hugo Souza Araujo <hsaraujo98@outlook.com>
2025-11-26 19:04:10 +0000

    1. Postagens Blog

--- Collections_Exercicios/postagem_blog/Blog.java
+++ Collections_Exercicios/postagem_blog/Blog.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class Blog {
@@ -8,6 +7,7 @@ public class Blog {
         this.postagens = new ArrayList<>();
     }
 
+    // Adiciona postagem, verificando duplicidade (mesmo autor + mesmo título)
     public void adicionarPostagem(Post postagem) throws Exception {
         if (postagens.contains(postagem)) {
             throw new Exception("Postagem jah existente");
@@ -15,22 +15,23 @@ public class Blog {
         postagens.add(postagem);
     }
 
+    // Retorna todos os autores ordenados pelo nome
     public Set<Autor> obterTodosAutores() {
-        Set<Autor> autores = new TreeSet<>();
-        for (Post p : postagens) {
-            autores.add(p.getAutor());
-        }
-        return autores;
+        return new TreeSet<>(postagens.stream()
+                .map(Post::getAutor)
+                .toList());
     }
 
+    // Retorna contagem por categoria, mantendo ordem natural do enum
     public Map<Categorias, Integer> obterContagemPorCategoria() {
-        Map<Categorias, Integer> contagem = new HashMap<>();
+        Map<Categorias, Integer> contagem = new EnumMap<>(Categorias.class);
         for (Post p : postagens) {
             contagem.put(p.getCategoria(), contagem.getOrDefault(p.getCategoria(), 0) + 1);
         }
         return contagem;
     }
 
+    // Retorna posts de um autor, ordenados pelo título
     public Set<Post> obterPostsPorAutor(Autor autor) {
         Set<Post> posts = new TreeSet<>();
         for (Post p : postagens) {
@@ -41,6 +42,7 @@ public class Blog {
         return posts;
     }
 
+    // Retorna posts de uma categoria, ordenados pelo título
     public Set<Post> obterPostsPorCategoria(Categorias categoria) {
         Set<Post> posts = new TreeSet<>();
         for (Post p : postagens) {
@@ -51,8 +53,9 @@ public class Blog {
         return posts;
     }
 
+    // Retorna todos os posts agrupados por categoria
     public Map<Categorias, Set<Post>> obterTodosPostsPorCategorias() {
-        Map<Categorias, Set<Post>> mapa = new HashMap<>();
+        Map<Categorias, Set<Post>> mapa = new EnumMap<>(Categorias.class);
         for (Post p : postagens) {
             mapa.putIfAbsent(p.getCategoria(), new TreeSet<>());
             mapa.get(p.getCategoria()).add(p);
@@ -60,8 +63,9 @@ public class Blog {
         return mapa;
     }
 
+    // Retorna todos os posts agrupados por autor
     public Map<Autor, Set<Post>> obterTodosPostsPorAutor() {
-        Map<Autor, Set<Post>> mapa = new HashMap<>();
+        Map<Autor, Set<Post>> mapa = new TreeMap<>();
         for (Post p : postagens) {
             mapa.putIfAbsent(p.getAutor(), new TreeSet<>());
             mapa.get(p.getAutor()).add(p);
