112fe71b08d273f72fbf4b58a1c0f9d7b8f80c72
Hugo Souza Araujo <hsaraujo98@outlook.com>
2025-11-25 23:59:57 -0300

    0. Mídias

---
+++ Generics/midias/Livro.java
@@ -0,0 +1,14 @@
+package midias;
+
+public class Livro extends Midia {
+    private int edicao;
+
+    public Livro(String nome, int edicao) {
+        super(nome);
+        this.edicao = edicao;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+}
